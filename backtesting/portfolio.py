b9f215d6a1ba107475d9d7310635c6a2a03028aa
sumukshashidhar <sumukuuu@gmail.com>
2021-03-27 23:01:55 +0530

    perf: changed list to a dicitionary to allow name access

--- backtesting/portfolio.py
+++ backtesting/portfolio.py
@@ -4,9 +4,10 @@ class Portfolio:
 	def __init__(self):
 		self.value = 0 # current value of the portfolio
 		self.injected = 0 # the value injected into the portfolio
-		self.pl = 0 # the proft-loss value
+		self.pl = 0 # the proft-loss values
+		self.plarray = []
 		self.liquid = 0 # liquid money to buy stocks and shares 
-		self.holdings = [] # filled with stocks and shares
+		self.holdings = {} # filled with stocks and shares
 		pass
 
 
@@ -22,7 +23,26 @@ class Portfolio:
 		# a way to obtain the entire value of our portfolio, with liquid and stocks
 		# v = value
 		v = self.liquid
-		for i in self.holdings:
-			v += i.get_value()
+		for i in self.holdings.keys():
+			v += self.holdings[i].get_value()
 
 		return v
+
+
+	
+
+		"""
+		Section needs to be planned before construction
+		"""
+
+	# def withdraw(self, requestamt):
+	# 	# allows you to withdraw money from your portfolio
+	# 	# this resets your pl statements as well, as the money in your account has changed
+	# 	if self.liquid >= requestamt:
+	# 		# enough funds exist for withdrawl
+	# 		self.plarray.append(self.pl)
+
+
+
+	# 	else:
+	# 		return False, "Not enough liquidity for withdrawl"
