1b103636977f1f90f4752b273f53e592ec81378f
sumukshashidhar <sumukuuu@gmail.com>
2021-03-27 23:33:16 +0530

    feat: add trader testing

--- backtesting/test.py
+++ backtesting/test.py
@@ -1,5 +1,16 @@
-from portfolio import Portfolio
+# from portfolio import Portfolio
 
-p = Portfolio()
-p.inject(500)
-print(p.get_value())
+# p = Portfolio()
+# p.inject(500)
+# print(p.get_value())
+
+
+from trader import Trader
+
+
+t = Trader()
+print(t.portfolio.liquid)
+print(t.buy('TICK', 5))
+print(t.sell('TICK', 1))
+print(t.get_holdings())
+print(t.get_pl())
